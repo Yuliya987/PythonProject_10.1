39c286e128324c3bde51924f27746864b0991e9b
unknown <stanislavovnajuli@yandex.ru>
2025-10-25 13:33:18 +0300

    a function for masking the card number and account number has been created

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+card_number = "7000792289606361"
+get_mask_card_number(card_number)
+
+
+account_number = "73654108430135874305"
+get_mask_account(account_number)
---
+++ src/masks.py
@@ -0,0 +1,24 @@
+card_number = input("Введите номер карты, 16 цифр: ")
+
+
+account_number = input("Введите номер счета: ")
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскировки номера карты."""
+    card_number = card_number.strip()
+    if len(card_number) != 16 or not card_number.isdigit():
+        return "Проверьте номер карты"
+    first_block = card_number[:4]
+    second_block = card_number[4:6]
+    last_block = card_number[-4:]
+    return f"{first_block} {second_block}** **** {last_block}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция маскировки номера счета."""
+    account_number = account_number.strip()
+    if len(account_number) != 20 or not account_number.isdigit():
+        return "Проверьте номер счета"
+    last_four = account_number[-4:]
+    return f"**{last_four}"
