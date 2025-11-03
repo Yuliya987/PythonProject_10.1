c619e4faf601e5392e5aa5ad6860f466d6de1462
Admin <stanislavovnajuli987@gmail.com>
2025-11-03 16:25:08 +0300

    import to main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-from src.widget import mask_account_card, get_date
-
 card_number = "7000792289606361"
 get_mask_card_number(card_number)
 
