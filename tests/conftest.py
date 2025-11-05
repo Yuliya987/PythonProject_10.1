cd43b0424b4a0a7fd053511594d80b8f5da35173
Admin <stanislavovnajuli987@gmail.com>
2025-11-06 00:55:33 +0300

    test_progressing.py has done

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,3 +21,10 @@ def valid_account_number_2():
 @pytest.fixture
 def valid_format_data():
     return "2024-03-11T02:26:18.671407"
+
+@pytest.fixture
+def list_dict_1():
+    return [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    ]
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,26 +1,42 @@
+import pytest
+
+from conftest import list_dict_1
 from src.processing import filter_by_state, sort_by_date
 
-def test_filter_by_state
-    assert
 
-def test_sort_by_date
-    assert
+@pytest.mark.parametrize("list_dict, expected_result", [
+    ([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+      {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+      {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}],
+     [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+])
+def test_filter_by_state(list_dict, expected_result):
+    assert filter_by_state(list_dict) == expected_result
 
-def test_filter_by_state_executed() -> None:
-    transactions = [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+
+def test_filter_by_state(list_dict_1):
+    assert filter_by_state(list_dict_1) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
-    expected = [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    ]
-    assert filter_by_state(transactions) == expected
 
-def test_sort_by_date_growing(transaction_data) -> None:  # Проверяем сортировку по возрастанию
-    sorted_transactions = sort_by_date(transaction_data, descending=False)
-    assert sorted_transactions[0]["id"] == 4  # Самая ранняя дата должна быть первой
-    assert sorted_transactions[1]["id"] == 2
-    assert sorted_transactions[2]["id"] == 3
-    assert sorted_transactions[3]["id"] == 1
+
+@pytest.mark.parametrize("list_dict, expected_result",
+                         [({"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                           {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                           {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                                  ], [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
+                          ]
+def test_sort_by_date(list_dict, expected_result):
+    assert sort_by_date(list_dict) == expected_result
+
+
+def test_sort_by_date(list_dict_1):
+    assert sort_by_date(list_dict_1) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
