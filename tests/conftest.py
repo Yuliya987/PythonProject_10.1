da5ec17059bab8400fe4b42c4a6d8dd0a534d830
Admin <stanislavovnajuli987@gmail.com>
2025-11-06 00:10:04 +0300

    test_widget.py has done

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,11 @@ def valid_card_number_2():
 @pytest.fixture
 def valid_account_number():
     return "73654108430135874305"
+
+@pytest.fixture
+def valid_account_number_2():
+    return "Счет 64686473678894779589"
+
+@pytest.fixture
+def valid_format_data():
+    return "2024-03-11T02:26:18.671407"
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,31 @@
+import pytest
+
+from conftest import valid_card_number_2, valid_account_number_2, valid_format_data
 from src.widget import mask_account_card, get_date
 
-def test_mask_account_card
-    assert
+
+@pytest.mark.parametrize("account_info, expected_result", [
+    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+    ("Visa 6831982476737658", "Visa 6831 98** **** 7658"),
+    ("Счет 64686473678894779589", "Счет **9589"),
+    ("", "Некорректный номер"),
+])
+def test_mask_account_card(account_info, expected_result):
+    assert mask_account_card(account_info) == expected_result
 
 
-def test_get_date
-    assert
+def test_valid_card_number(valid_card_number_2):  # правильный ввод карты
+    assert mask_account_card(valid_card_number_2) == "Visa 1234 56** **** 3456"
+
+
+def test_valid_card_number_(valid_account_number_2):  # правильный ввод со словом счёт
+    assert mask_account_card(valid_account_number_2) == "Счет **9589"
+
+
+@pytest.mark.parametrize("format_data, expected_result", [
+    ("2024-03-11T02:26:18.671407", "11.03.2024"),
+    ("2025-06-28T02:26:18.671407", "28.06.2025"),
+    ("", "")
+])
+def test_get_date(valid_format_data):
+    assert get_date(valid_format_data) == "11.03.2024"
