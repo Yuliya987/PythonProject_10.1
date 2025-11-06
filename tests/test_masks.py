3cb997f068c977efd9338cf64b65314d6d90e00d
Admin <stanislavovnajuli987@gmail.com>
2025-11-06 20:17:22 +0300

    tests coverage not ready yet

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,18 @@
 import pytest
+from conftest import valid_account_number, valid_card_number
 
-from conftest import valid_card_number, valid_account_number
 from src.masks import get_mask_account, get_mask_card_number
 
 
-@pytest.mark.parametrize("account_number, expected_result", [
-    ("73654108430135874305", "**4305"),
-    ("12479812478964124781", "**4781"),
-    ("12345667788991010011", "**0011"),
-    ("", "Некорректный номер счёта"),
-])
+@pytest.mark.parametrize(
+    "account_number, expected_result",
+    [
+        ("73654108430135874305", "**4305"),
+        ("12479812478964124781", "**4781"),
+        ("12345667788991010011", "**0011"),
+        ("", "Некорректный номер счёта"),
+    ],
+)
 def test_get_mask_account(account_number, expected_result):
     assert get_mask_account(account_number) == expected_result
 
@@ -29,11 +32,14 @@ def test_account_number_is_missing():
     assert str(exc_info.value) == "Некорректный номер счёта"
 
 
-@pytest.mark.parametrize("card_number, expected_result", [
-    ("7000792289606361", "7000 79** **** 6361"),
-    ("1596837868705199", "1596 83** **** 5199"),
-    ("", "Некорректный номер карты"),
-])
+@pytest.mark.parametrize(
+    "card_number, expected_result",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("", "Некорректный номер карты"),
+    ],
+)
 def test_get_mask_card_number(card_number, expected_result):
     assert get_mask_account(card_number) == expected_result
 
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,15 +1,17 @@
 import pytest
 
-from conftest import valid_card_number_2, valid_account_number_2, valid_format_data
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize("account_info, expected_result", [
-    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-    ("Visa 6831982476737658", "Visa 6831 98** **** 7658"),
-    ("Счет 64686473678894779589", "Счет **9589"),
-    ("", "Некорректный номер"),
-])
+@pytest.mark.parametrize(
+    "account_info, expected_result",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Visa 6831982476737658", "Visa 6831 98** **** 7658"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("", "Некорректный номер"),
+    ],
+)
 def test_mask_account_card(account_info, expected_result):
     assert mask_account_card(account_info) == expected_result
 
@@ -22,10 +24,9 @@ def test_valid_card_number_(valid_account_number_2):  # правильный в�
     assert mask_account_card(valid_account_number_2) == "Счет **9589"
 
 
-@pytest.mark.parametrize("format_data, expected_result", [
-    ("2024-03-11T02:26:18.671407", "11.03.2024"),
-    ("2025-06-28T02:26:18.671407", "28.06.2025"),
-    ("", "")
-])
+@pytest.mark.parametrize(
+    "format_data, expected_result",
+    [("2024-03-11T02:26:18.671407", "11.03.2024"), ("2025-06-28T02:26:18.671407", "28.06.2025"), ("", "")],
+)
 def test_get_date(valid_format_data):
     assert get_date(valid_format_data) == "11.03.2024"
