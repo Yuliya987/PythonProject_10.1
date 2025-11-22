f4a690d5c58998aa9e29cdbfd998f6dd3484b1b8
Admin <stanislavovnajuli987@gmail.com>
2025-11-22 15:03:21 +0300

    homework 11.2 finish refactoring

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,10 +3,8 @@ from src.decorators import log
 
 def test_log_2(capsys):
     @log(None)
-    def my_function():
-        result = my_function()
-        assert result == "Expected Result"
-        return result
+    def my_function(x, y):
+        return x + y
 
 
     my_function(1, 2)
@@ -16,10 +14,8 @@ def test_log_2(capsys):
 
 def test_log():
     @log(filename="mylog.txt")
-    def my_function():
-        result = my_function()
-        assert result == "Expected Result"
-        return result
+    def my_function(x, y):
+        return x + y
 
     my_function()
     with open("mylog.txt", "a") as file:
