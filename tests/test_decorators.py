6f66957847d16f6fa7dc7d6e2f9069d426795e20
Admin <stanislavovnajuli987@gmail.com>
2025-11-22 10:31:24 +0300

    homework 11.2 refactored

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,21 +3,26 @@ from src.decorators import log
 
 def test_log_2(capsys):
     @log(None)
-    def my_function(x, y):
-        return x + y
+    def my_function():
+        result = my_function()
+        assert result == "Expected Result"
+        return result
+
 
     my_function(1, 2)
     captured = capsys.readouterr()
     assert "my_function ok" in captured.out
 
 
-def test_log(x,y):
+def test_log():
     @log(filename="mylog.txt")
-    def my_function(x, y):
-        return x + y
+    def my_function():
+        result = my_function()
+        assert result == "Expected Result"
+        return result
 
-    my_function(1, 2)
-    with open("mylog.txt", "r") as file:
+    my_function()
+    with open("mylog.txt", "a") as file:
         log_content = file.read()
 
     assert "my_function ok" in log_content
