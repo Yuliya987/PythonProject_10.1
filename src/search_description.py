cb6a06165bf022c73b13641769ae3ee66b7abc91
Admin <stanislavovnajuli987@gmail.com>
2026-01-04 18:55:21 +0300

    homework 13_2 refactored2

--- src/search_description.py
+++ src/search_description.py
@@ -15,21 +15,10 @@ def process_bank_search(transactions:list[dict], search:str)->list[dict]:
     return filtered_transactions
 
 
-def process_bank_operations(transactions:list[dict], categories:list)->dict
+def process_bank_operations(transactions:list[dict], categories:list)->dict:
     counter = Counter()
-    for op in transactions
+    for op in transactions:
         category = op.get('description', '').strip()
         if category in categories:
             counter[category] += 1
     return dict(counter)
-
-# Пример использования:
-operations = [
-    {'id': 1, 'description': 'Пополнение счета на 1000 рублей'},
-    {'id': 2, 'description': 'Снятие 5000 с карты'},
-    {'id': 3, 'description': 'Перевод на счет 2000'},
-    {'id': 4, 'description': 'Оплата услуг связи'}
-]
-
-result = filter_operations_by_description(operations, 'перевод')
-print(result)  # Выведет операции с описанием, содержащим слово "перевод"
--- tests/test_search_description.py
+++ tests/test_search_description.py
@@ -4,17 +4,20 @@ from src.search_description import process_bank_search, process_bank_operations
 
 
 @pytest.mark.parametrize(
-    "account_number, expected_result",
-
-def test_process_bank_search(
-
-operations = [
-    {'id': 1, 'description': 'Пополнение счета на 1000 рублей'},
-    {'id': 2, 'description': 'Снятие 5000 с карты'},
-    {'id': 3, 'description': 'Перевод на счет 2000'},
-    {'id': 4, 'description': 'Оплата услуг связи'}
-]
-    result = process_bank_search(operations, 'перевод')
+    "search_, expected_result",
+    [
+        ("перевод", [{'description': 'Перевод на счет 2000', 'id': 3 }]),
+        ("оплата", [{'description': 'Оплата услуг связи', 'id': 4}]),
+        ("пополнение", [{'description': 'Пополнение счета на 1000 рублей', 'id': 1}]),
+        ("снятие", [{'description': 'Снятие 5000 с карты', 'id': 2}]),
+    ],
+)
 
 
-print(result)  # Выведет операции с описанием, содержащим слово "перевод"
+def test_process_bank_search(transactions_1, search_, expected_result):
+    assert process_bank_search(transactions_1, search_) == expected_result
+
+
+def test_process_bank_operations(transactions_1):
+    result = process_bank_operations(transactions_1, categories=["Перевод на счет 2000"])
+    assert result == {"Перевод на счет 2000": 1}
